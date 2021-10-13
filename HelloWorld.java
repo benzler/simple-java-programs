701389d2ee8173691fc2a2f8b33542332aa06c06
Andrin Benz <58980760+benzler@users.noreply.github.com>
2021-10-13 13:46:40 +0200

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ * Erste Übungsaufgabe zum Thema Verändern von Ausgaben und Miteinbeziehen von Git Repositorys
+ * Author: Andrin Benz
+ */
+public class HelloProgrammer {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+    }
+}
